7308c833bd94d48fcd94196cc270432cf1bce02c
Arturo Burela <uno@arturoburela.com>
2018-11-08 15:49:29 -0600

    Advances

--- game.js
+++ game.js
@@ -1,8 +1,44 @@
 // assigning to exports will not modify module, must use module.exports
 module.exports = class Game {
-  constructor(password) {
-    this.password = password;
+  constructor() {
+    // Array of players
     this.players = [];
+    this.tiles = [];
+    this.tiles.push({l1: 0, l2: 0});
+    this.tiles.push({l1: 0, l2: 1});
+    this.tiles.push({l1: 0, l2: 2});
+    this.tiles.push({l1: 0, l2: 3});
+    this.tiles.push({l1: 0, l2: 4});
+    this.tiles.push({l1: 0, l2: 5});
+    this.tiles.push({l1: 0, l2: 6});
+    this.tiles.push({l1: 1, l2: 1});
+    this.tiles.push({l1: 1, l2: 2});
+    this.tiles.push({l1: 1, l2: 3});
+    this.tiles.push({l1: 1, l2: 4});
+    this.tiles.push({l1: 1, l2: 5});
+    this.tiles.push({l1: 1, l2: 6});
+    this.tiles.push({l1: 2, l2: 2});
+    this.tiles.push({l1: 2, l2: 3});
+    this.tiles.push({l1: 2, l2: 4});
+    this.tiles.push({l1: 2, l2: 5});
+    this.tiles.push({l1: 2, l2: 6});
+    this.tiles.push({l1: 3, l2: 3});
+    this.tiles.push({l1: 3, l2: 4});
+    this.tiles.push({l1: 3, l2: 5});
+    this.tiles.push({l1: 3, l2: 6});
+    this.tiles.push({l1: 4, l2: 4});
+    this.tiles.push({l1: 4, l2: 5});
+    this.tiles.push({l1: 4, l2: 6});
+    this.tiles.push({l1: 5, l2: 5});
+    this.tiles.push({l1: 5, l2: 6});
+    this.tiles.push({l1: 6, l2: 6});
+  }
+
+  shuffletiles() {
+    for (let i = this.tiles.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.tiles[i], this.tiles[j]] = [this.tiles[j], this.tiles[i]];
+    }
   }
 
   addPlayer(){
@@ -11,7 +47,7 @@ module.exports = class Game {
     });
   }
 
-  area() {
+  nextTurn() {
     return this.width ** 2;
   }
 };
--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+var compression = require('compression');
 var express = require('express'); // Get the module
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var game = require('./game.js');
+var HashMap = require('hashmap');
+// Use compression
+app.use(compression());
 // Serve static assets
 app.use('/game', express.static('public'));
 // HTML only to test socket
@@ -14,34 +18,42 @@ app.get('/', function(req, res){
 io.use(function(socket, next) {
   console.log(socket.id);
   // Use socket id as game password
-  socket.game = new game(socket.id);
-  var handshakeData = socket.request;
-  // make sure the handshake data looks good as before
-  // if error do this:
-    // next(new Error('not authorized'));
-  // else just call next
+  //var handshakeData = socket.request;
   next();
 });
+
+// Games in memory
+var games = new HashMap();
 
 
 // connection
 io.on('connection', function(socket){
 
-  //Create new game with password
+  //Create new game
   socket.on('host game', function (password) {
-    console.log(gameinstance.password);
+    console.log('Alojando juego, ID: ' + socket.id + ' op pass: ' + password);
+    games.set(socket.id , new game());
   });
 
   //Join other player game
   socket.on('join game', function (room) {
-    console.log(room);
-    //Add player to host room
-    socket.join(room);
+    // Verify game exists
+    if (games.has(room)) {
+      //Add player to host room
+      socket.join(room);
+      console.log(socket.id + " joined to room: " + room);
+      // Notify players of joined player
+      socket.broadcast.to(room).emit('player connected', 'Se ha conectado el jugador: ' + socket.id);
+    } else {
+      // Error no game
+
+    }
   });
 
   // Iniciar juego
   socket.on('start game', function(msg){
-    socket.broadcast.to(socket.id).emit('my message', msg);
+    console.log('Game started');
+    io.broadcast.to(socket.id).emit('start game', msg);
   });
 
 });
